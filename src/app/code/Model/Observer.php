ca7bb07e16cd45fb9badc1f8c6d14dc4e4437ddc
Alessandro Ronchi <aleron75@gmail.com>
2013-07-11 18:39:48 +0200

    cleaned some code

--- src/app/code/Model/Observer.php
+++ src/app/code/Model/Observer.php
@@ -4,17 +4,15 @@ class Webgriffe_ProductDelete_Model_Observer
     public function onProductDeleteAfter($observer)
     {
         $product = $observer->getEvent()->getDataObject();
-
-        Mage::log('Observed a delete after commit on product ' . $product->getId(), null, 'Webgriffe_ProductDelete.log', true);
+        Mage::log('Observed a delete after commit on product ' . $product->getId(), null, 'Webgriffe_ProductDelete.log');
         if ($product->getId()) {
-            #$images = $product->getMediaGalleryImages();
             $images = $product->getMediaGallery('images');
             foreach ($images as $image) {
                 $imageFile = Mage::getBaseDir('media').DS.'catalog'.DS.'product'.$image['file'];
                 if (@unlink($imageFile)) {
-                    Mage::log('Deleted Product Image File "' . $imageFile . '"', null, 'Webgriffe_ProductDelete.log', true);
+                    Mage::log('Deleted Product Image File "' . $imageFile . '"', null, 'Webgriffe_ProductDelete.log');
                 } else {
-                    Mage::log('Could not deleted Product Image File "' . $imageFile . '"', null, 'Webgriffe_ProductDelete.log', true);
+                    Mage::log('Could not deleted Product Image File "' . $imageFile . '"', null, 'Webgriffe_ProductDelete.log');
                 }
             }
         }
